3da6f4ced50dec1513a1f74dccbf14695587dc4f
lvnh2003 <your-email@example.com>
2024-11-17 21:12:13 +0700

    refactor : move function main to classify

--- README.md
+++ README.md
@@ -3,13 +3,28 @@
 ```
     Smart-Sport-Camera/
 ├── data/            # Dữ liệu
-├── scr/            # Mã nguồn chính
-│   ├── __init__.py
-│   ├── preprocessing/     # Xử lý dữ liệu đầu vào
+│   ├── cameras.txt
+├── src/            # Mã nguồn chính
 │   ├── models/            # Các mô hình
+│   │   ├── best.pt
+│   ├── preprocessing/     # Xử lý dữ liệu đầu vào
+│   │   ├── classify.py
+│   ├── ui
 │   └── utils/             # Các hàm tiện ích      
+│       ├── config.py
 ├── requirements.txt
 ├── README.md
 └── .gitignore
 ```
 
+### Các trường hợp đáng chú ý
+```
+1.Có sự xuất hiện của các cầu thủ và bóng trên sân
+    Team Left + Team Right >= 2 and Ball > 0
+2.Có cầu thủ và trọng tài xuất hiện gần nhau
+    Main Ref > 1 and Team Left + Team Right > 0
+3.Thủ môn và bóng xuất hiện gần nhau
+    Ball > 0 and ( GoalKeeper Left > 0 or GoalKeeper Right >0 )
+4. Các tình huống gây tranh cãi (vào bóng mạnh hoặc va chạm)
+    Team Left + Team Right > 3 and Ball > 0:
+```
--- src/preprocessing/classify.py
+++ src/preprocessing/classify.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-import sys
-from ultralytics import YOLO
 from sklearn.cluster import KMeans
+from src.utils.config import box_colors, labels
+from ultralytics import YOLO
 def get_grass_color(img):
     """
     Finds the color of the grass in the background of the image
@@ -173,3 +173,80 @@ def get_left_team_label(players_boxes, kits_colors, kits_clf):
     left_team_label = 1
 
   return left_team_label
+def annotate_video(video_path, model):
+    """
+    Loads the input video and runs the object detection algorithm on its frames, finally it annotates the frame with
+    the appropriate labels
+
+    Args:
+        video_path: String the holds the path of the input video
+        model: Object that represents the trained object detection model
+    Returns:
+    """
+    cap = cv2.VideoCapture(video_path)
+
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+    kits_clf = None
+    left_team_label = 0
+    grass_hsv = None
+    while cap.isOpened():
+        # Read a frame from the video
+        success, frame = cap.read()
+
+        current_frame_idx = cap.get(cv2.CAP_PROP_POS_FRAMES)
+        if success:
+            # if current_frame_idx % frame_interval != 0:
+            #     continue
+            # Run YOLOv8 inference on the frame
+            annotated_frame = cv2.resize(frame, (width, height))
+            result = model(annotated_frame, conf=0.5, verbose=False)[0]
+            print(len(result.boxes))
+            # Get the players boxes and kit colors
+            players_imgs, players_boxes = get_players_boxes(result)
+            kits_colors = get_kits_colors(players_imgs, grass_hsv, annotated_frame)
+
+            # Run on the first frame only
+            if current_frame_idx == 1:
+                kits_clf = get_kits_classifier(kits_colors)
+                left_team_label = get_left_team_label(players_boxes, kits_colors, kits_clf)
+                grass_color = get_grass_color(result.orig_img)
+                grass_hsv = cv2.cvtColor(np.uint8([[list(grass_color)]]), cv2.COLOR_BGR2HSV)
+
+            for box in result.boxes:
+                label = int(box.cls.numpy()[0])
+                x1, y1, x2, y2 = map(int, box.xyxy[0].numpy())
+
+                # If the box contains a player, find to which team he belongs
+                if label == 0:
+                    kit_color = get_kits_colors([result.orig_img[y1: y2, x1: x2]], grass_hsv)
+                    team = classify_kits(kits_clf, kit_color)
+                    if team == left_team_label:
+                        label = 0
+                    else:
+                        label = 1
+
+                # If the box contains a Goalkeeper, find to which team he belongs
+                elif label == 1:
+                    if x1 < 0.5 * width:
+                        label = 2
+                    else:
+                        gk_label = 3
+
+                # Increase the label by 2 because of the two add labels "Player-L", "GK-L"
+                else:
+                    label = label + 2
+
+                cv2.rectangle(annotated_frame, (x1, y1), (x2, y2), box_colors[str(label)], 2)
+                cv2.putText(annotated_frame, labels[label], (x1 - 30, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
+                            box_colors[str(label)], 2)
+
+
+            cv2.imshow("Result",annotated_frame)
+            cv2.waitKey(1)
+        else:
+            # Break the loop if the end of the video is reached
+            break
+    cap.release()
+    cv2.destroyAllWindows()
