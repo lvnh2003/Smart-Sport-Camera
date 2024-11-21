a94ccbfab133cd1f74f8f1b38d5c7f48c003669e
lvnh2003 <nguyenngoclanh16052003@gmail.com>
2024-11-21 15:47:42 +0700

    fix : run UI

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from PyQt5.QtWidgets import QApplication
+
 from src.preprocessing.classify import *
+from src.components.load_ui import LoadUI
 if __name__ == "__main__":
     model = YOLO("./src/models/best.pt")
     video_path = "./data/test.mp4"
-    annotate_video(video_path, model)
+    # annotate_video(video_path, model)
+    app = QApplication([])
+    mainwindow = LoadUI(model)
+    mainwindow.show()
+    app.exec_()
---
+++ src/components/thread_camera.py
@@ -0,0 +1,32 @@
+from PyQt5.QtCore import pyqtSignal, QThread
+import numpy as np,cv2
+
+
+class ThreadCamera(QThread):
+    ImageUpdate = pyqtSignal(np.ndarray)
+    def __init__(self,model):
+        super().__init__()
+        self.model = model
+
+    global camIndex
+    # run camera
+    def run(self):
+        Capture = cv2.VideoCapture(0)
+        Capture.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+        Capture.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+        self.ThreadActive = True
+        while self.ThreadActive:
+            ret,frame_cap = Capture.read()
+            if ret:
+                results =  self.model(frame_cap, conf=0.5, verbose=False)
+                if len(results[0]) > 0:
+                    box = results[0].boxes.xyxy[0]
+
+                    print("SOS")
+
+                annotated_frame = results[0].plot()
+                self.ImageUpdate.emit(annotated_frame)
+
+    def stop(self):
+        self.ThreadActive = False
+        self.quit()
