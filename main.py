d8def53fba1d894f98a2741e6371b566eaaf1ebf
lvnh2003 <nguyenngoclanh16052003@gmail.com>
2024-11-21 21:28:49 +0700

    classification of player on the field

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from PyQt5.QtWidgets import QApplication
-
-from src.preprocessing.classify import *
 from src.components.load_ui import LoadUI
+from ultralytics import YOLO
 if __name__ == "__main__":
     model = YOLO("./src/models/best.pt")
     video_path = "./data/test.mp4"
-    # annotate_video(video_path, model)
     app = QApplication([])
     mainwindow = LoadUI(model)
     mainwindow.show()
