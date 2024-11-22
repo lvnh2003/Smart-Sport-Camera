b7e3da7721b4ce585d835e604033fecf14d4315f
lvnh2003 <nguyenngoclanh16052003@gmail.com>
2024-11-22 23:43:47 +0700

    feature : shows the notable actions of the players

--- src/components/thread_camera.py
+++ src/components/thread_camera.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import pyqtSignal, QThread
 import numpy as np,cv2
+
 from src.preprocessing.classify import  get_players_boxes, get_player_classifier, get_left_team_label, \
     get_grass_color, classify_players, get_player_colors
 from src.utils.config import box_colors, labels
@@ -27,7 +28,7 @@ class ThreadCamera(QThread):
             ret,frame_cap = Capture.read()
             current_frame_idx = Capture.get(cv2.CAP_PROP_POS_FRAMES)
             if ret:
-                result =  self.model(frame_cap, conf=0.5, verbose=False)[0]
+                result =  self.model(frame_cap, conf=0.5,verbose = False)[0]
                 players_imgs, players_boxes = get_players_boxes(result)
                 players_colors = get_player_colors(players_imgs, grass_hsv, frame_cap)
                 # chạy 1 frame đầu tiên thôi
@@ -36,42 +37,75 @@ class ThreadCamera(QThread):
                     left_team_label = get_left_team_label(players_boxes, players_colors, players_clf)
                     grass_color = get_grass_color(result.orig_img)
                     grass_hsv = cv2.cvtColor(np.uint8([[list(grass_color)]]), cv2.COLOR_BGR2HSV)
-                count_team_left = 0
-                count_team_right = 0
-                count_gk_left = 0
-                count_gk_right = 0
-                count_ball = 0
-                count_main_ref  = 0
+                counts = {
+                    "team_left": 0, "team_right": 0,
+                    "gk_left": 0, "gk_right": 0,
+                    "ball": 0, "main_ref": 0
+                }
+                positions = {"ball": None, "ref": None, "players": [], "goalkeepers": []}
                 for box in result.boxes:
                     label = int(box.cls.cpu().numpy()[0])
                     x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())
-
+                    center_position = ((x1 + x2) // 2, (y1 + y2) // 2)
                     #Nếu trong box có chứa cầu thủ thì tìm người đó thuộc team nào
 
                     if label == 0:
                         player_color = get_player_colors([result.orig_img[y1: y2, x1: x2]], grass_hsv)
                         team = classify_players(players_clf, player_color)
                         if team == left_team_label:
-                            count_team_left += 1
+                            counts["team_left"] += 1
                             label = 0
                         else:
+                            counts["team_right"] += 1
                             label = 1
-                            count_team_right += 1
+                        positions["players"].append(center_position)
                     # Nếu trong box có chứa thủ môn thì tìm người đó thuộc team nào
                     elif label == 1:
                         if x1 < 0.5 * 300:
                             label = 2
-                            count_gk_left += 1
+                            counts["gk_left"] += 1
                         else:
-                            count_gk_right += 1
-                    # Tăng nhãn lên 2 vì có thêm hai nhãn "Player-L", "GK-L"
+                            label = 3
+                            counts["gk_right"] += 1
+                        positions["goalkeepers"].append(center_position)
+                    elif label == 2:
+                        counts["ball"] += 1
+                        positions["ball"] = center_position
+                        label = 4
+                    elif label == 3:
+                        counts["main_ref"] += 1
+                        positions["ref"] = center_position
+                        label = 5
+                    # Tăng nhãn lên 2 vì có thêm hai nhãn "Player-L", "GK-L", các classes còn lại không sử dụng
                     else:
                         label = label + 2
-                        count_ball += 1
+
                     cv2.rectangle(frame_cap, (x1, y1), (x2, y2), box_colors[str(label)], 2)
                     cv2.putText(frame_cap, labels[label], (x1 - 30, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
                                 box_colors[str(label)], 2)
                 # Nếu đạt các điều kiện thì hiện thị trên camera chính
+                is_noticeable = False
+
+                # Thủ môn và bóng xuất hiện gần nhau
+                if counts["ball"] > 0 and (counts["gk_left"] > 0 or counts["gk_right"] > 0):
+                    is_noticeable = True
+
+                # Cầu thủ và trọng tài gần nhau!
+                if counts["main_ref"] > 0 and positions["ref"] is not None:
+                    for player_pos in positions["players"]:
+                        if np.linalg.norm(np.array(positions["ref"]) - np.array(player_pos)) < 50:
+                            is_noticeable = True
+
+                # Tình huống tranh chấp xảy ra
+                if counts["team_left"] + counts["team_right"] > 3 and counts["ball"] == 0:
+                    is_noticeable = True
+
+                # Có cầu thủ và bóng trên sân!
+                if counts["team_left"] + counts["team_right"] >= 2 and counts["ball"] > 0:
+                    is_noticeable = True
+
+                if(is_noticeable):
+                    self.ImageDisplayMain.emit(frame_cap)
                 self.ImageUpdate.emit(frame_cap)
 
 
